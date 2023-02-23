afee6cae449a9108306f982019794c8c6d51ea86
mathieu ralambotsirofo <m.ralambotsirofo@gmail.com>
2023-02-23 17:00:30 +0100

    back end almost done.display the content

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params["bookmark"][:movie_id])
+    @list = List.find(params["bookmark"][:list_id])
+    @bookmark.list_id = @list.id
+    @bookmark.movie_id = @movie.id
+    @bookmark.save
+
+    redirect_to list_path(params[:list_id])
+    raise
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'bookmarks/comment'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
+
+# Prefix Verb   URI Pattern                                                                                       Controller#Action
+# bookmarks_comment GET    /bookmarks/comment(.:format)                                                                      bookmarks#comment
+#    list_bookmarks POST   /lists/:list_id/bookmarks(.:format)                                                               bookmarks#create
+# new_list_bookmark GET    /lists/:list_id/bookmarks/new(.:format)                                                           bookmarks#new
+#     list_bookmark DELETE /lists/:list_id/bookmarks/:id(.:format)                                                           bookmarks#destroy
+#             lists GET    /lists(.:format)                                                                                  lists#index
+#                   POST   /lists(.:format)                                                                                  lists#create
+#          new_list GET    /lists/new(.:format)                                                                              lists#new
+#              list GET    /lists/:id(.:format)                                                                              lists#show
